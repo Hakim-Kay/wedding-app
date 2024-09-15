659c7c9610e0b4212238786fbe8c27b2f573d6c3
Hakim-Kay <hakim.kayira@gmail.com>
2024-09-15 15:52:53 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import withPWA from 'next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+export default pwaConfig(nextConfig);
