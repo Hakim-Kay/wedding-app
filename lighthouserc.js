401ea899afac1aa16ea7d2573d448fa70e24a43e
Hakim-Kay <hakim.kayira@gmail.com>
2024-09-20 18:19:37 +0100

    [FIX]- Update lighthouserc.js file

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,10 @@
 module.exports = {
     ci: {
+        collect: {
+            staticDistDir: '.next',
+            url: ['/'],
+            startServerCommand: 'npm run start',
+        },
         upload: {
             target: 'temporary-public-storage',
         },
