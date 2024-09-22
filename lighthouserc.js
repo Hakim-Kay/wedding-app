2f0a3beb1d61a4bbd383a94c92ef40714308cb42
Hakim-Kay <hakim.kayira@gmail.com>
2024-09-22 17:00:10 +0100

    Removed Chrome download & Updated lighthouserc.js file

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,19 @@
 module.exports = {
     ci: {
         collect: {
-            staticDistDir: '.next',
-            url: ['http://localhost:3000'],
             startServerCommand: 'npm run start',
+            startServerReadyPattern: 'ready on',
+            url: [
+                'http://localhost:3000/',
+                // Add other important routes here
+            ],
+            numberOfRuns: 3,
         },
         upload: {
             target: 'temporary-public-storage',
+        },
+        assert: {
+            preset: 'lighthouse:recommended',
         },
     },
 };
